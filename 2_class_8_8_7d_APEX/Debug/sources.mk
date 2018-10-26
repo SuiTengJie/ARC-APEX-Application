c1152f9fafb4040b29f0f86f71f1de214490b677
tengjie <779651329@qq.com>
2018-10-26 11:54:30 +0800

    add the metaware projects: 2_class_8_8_7d and 2_class_8_8_7d_APEX

---
+++ 2_class_8_8_7d_APEX/Debug/sources.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+A_SRCS := 
+MET_SRCS := 
+CXX_SRCS := 
+CL_SRCS := 
+C++_SRCS := 
+S_SRCS := 
+LIB_SRCS := 
+CC_SRCS := 
+ASM_SRCS := 
+CPP_SRCS := 
+C_SRCS := 
+O_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+CL_DEPS := 
+EXECUTABLES := 
+CXX_DEPS := 
+OBJS := 
+ASM_DEPS := 
+MAPFILE := 
+S_DEPS := 
+MET_DEPS := 
+CPP_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+
---
+++ 2_class_8_8_7d_APEX/Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+$(PROJECT)/main.c 
+
+OBJS += \
+./main.o 
+
+C_DEPS += \
+./depend/main.u 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MetaWare ARC EM C/C++ Compiler'
+	ccac -c -Xdsp_divsqrt=radix2 -Hinclude="C:/Users/tengjie/mide/workspace/2_class_8_8_7d_APEX/apexextensions.h" -Xswap -Xbitscan -arcv2em -Xmpy_option=mpyd -Xdsp2 -Hpc_width=32 -Xcode_density -Hccm -Xtimer0 -Xtimer1 -dmac -Xdsp_complex -Xbarrel_shifter -core2 -Xshift_assist -g -Xdiv_rem=radix2 -Hnocopyr -Xstack_check -I"$(PROJECT)" -Xdsp_accshift=limited -Humake -Hdepend="depend" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
